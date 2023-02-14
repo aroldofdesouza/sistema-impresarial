e16ac805ad4d22c73241d0e06c982962139a1ed5
Aroldo F de Souza <aroldofdesouza@gmail.com>
2023-02-14 18:58:00 -0300

    Consulta de Venda por Data concluida

--- backup/uconsultavendas.pas
+++ backup/uconsultavendas.pas
@@ -18,9 +18,12 @@ type
     DBGrid1: TDBGrid;
     Label1: TLabel;
     Label2: TLabel;
+    txtValorTotalListado: TLabel;
     Panel1: TPanel;
     btnBuscar: TSpeedButton;
     procedure btnBuscarClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormShow(Sender: TObject);
   private
 
   public
@@ -39,9 +42,31 @@ uses uDM;
 { TFConsultaVendas }
 
 procedure TFConsultaVendas.btnBuscarClick(Sender: TObject);
+var
+  valor_total_listado : Double;
 begin
-  DM.TVenda.Filter := 'DATA >= '+QuotedStr(StrToDateTime(DateTimePicker1.Date))+' AND DATA <= '+QuotedStr(StrToDateTime(DateTimePicker2.Date));
+  DM.TVenda.Filter := 'DATA >= '+QuotedStr(DateTimeToStr(DateTimePicker1.Date))+' AND DATA <= '+QuotedStr(DateTimeToStr(DateTimePicker2.Date));
   DM.TVenda.Filtered := true;
+
+  valor_total_listado := 0;
+  DM.TVenda.First;
+  while not(DM.TVenda.EOF)do
+  begin
+    valor_total_listado:= valor_total_listado + DM.TVendaVALOR_TOTAL.Value;
+    DM.TVenda.Next;
+  end;
+  txtValorTotalListado.Caption := 'Valor Total Listado: '+FormatFloat('R$###,###,##0.00', valor_total_listado);
+end;
+
+procedure TFConsultaVendas.FormClose(Sender: TObject;
+  var CloseAction: TCloseAction);
+begin
+  DM.TVenda.Filtered := false;
+end;
+
+procedure TFConsultaVendas.FormShow(Sender: TObject);
+begin
+  btnBuscar.Click;
 end;
 
 end.
